d38849cbf93d7db6386eff21e544fd45df8c1fac
ACGMN <ootopoo@vip.qq.com>
2023-06-09 20:07:46 +0800

    Update
    Signed-off-by: ACGMN <ootopoo@vip.qq.com>

--- Source/ActionRoguelike/ActionRoguelike.Build.cs
+++ Source/ActionRoguelike/ActionRoguelike.Build.cs
@@ -6,28 +6,29 @@ public class ActionRoguelike : ModuleRules
 {
 	public ActionRoguelike(ReadOnlyTargetRules Target) : base(Target)
 	{
+		CppStandard = CppStandardVersion.Latest;
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
 		PublicDependencyModuleNames.AddRange(new string[]
 		{
-			"Core", 
-			"CoreUObject", 
-			"Engine", 
-			"InputCore", 
-			"AIModule", 
-			"GameplayTasks", 
-			"UMG", 
-			"GameplayTags", 
-			"OnlineSubsystem", 
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"AIModule",
+			"GameplayTasks",
+			"UMG",
+			"GameplayTags",
+			"OnlineSubsystem",
 			"DeveloperSettings",
 			"SignificanceManager"
 		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
